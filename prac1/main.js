0813614efd776e8325b70fd8905a2a54fca07c50
davidroses02 <davidrosasalcaraz@gmail.com>
2021-10-26 10:18:32 +0200

    eaa

--- prac1/main.js
+++ prac1/main.js
@@ -9,71 +9,76 @@ function cargaContextoCanvas(idCanvas) {
     return false;
 }
 
+let i = 0;
+
 window.onload = function () {
     
     let contexto = cargaContextoCanvas('micanvas');
     if (contexto) {
         
-        contexto.fillStyle = '#800099';
-        contexto.strokeRect(107, 65, 10, 10);
-        contexto.strokeRect(43, 65, 10, 10);
+        //contexto.fillStyle = '#800099';
+        coche(contexto,0);
+        setInterval(coche,50, contexto);
         
-        
-        //contexto.rotate(200,75);
-        //moverRueda(contexto);
-
-        contexto.beginPath();
-        
-        contexto.moveTo(25,25);
-        contexto.lineTo(100,25);
-        
-        contexto.moveTo(25,25);
-        contexto.lineTo(20,45);
-        
-        contexto.moveTo(20,45);
-        contexto.lineTo(5,45);
-
-        contexto.moveTo(5,45);
-        contexto.lineTo(5,70);
-
-        contexto.moveTo(5,70);
-        contexto.lineTo(35,70);
-
-        contexto.moveTo(60,70);
-        contexto.lineTo(100,70);
-
-        contexto.moveTo(125,70);
-        contexto.lineTo(160,70);
-
-        contexto.moveTo(160,70);
-        contexto.lineTo(165,60);
-
-        contexto.moveTo(165,60);
-        contexto.lineTo(165,50);
-
-        contexto.moveTo(165,50);
-        contexto.lineTo(115,40);
-
-        contexto.moveTo(115,40);
-        contexto.lineTo(93,40);
-
-        contexto.moveTo(93,40);
-        contexto.lineTo(100,25);
-
-        //contexto.moveTo(60,70);
-        
-        contexto.arc(48,70,12,0,Math.PI*2,true);
-        //contexto.fill();
-        contexto.stroke();
-        contexto.beginPath();
-        contexto.arc(112,70,12,0,Math.PI*2,true);
-        //contexto.fill();
-        contexto.stroke();
-
-        contexto.closePath();
-        contexto.stroke();
-       
-    }
-
+    }   
     
 }
+
+function coche(contexto) {
+
+    contexto.clearRect(0, 0, 800, 800);
+    
+    contexto.strokeRect(107+i, 65, 10, 10);
+        contexto.strokeRect(43+i, 65, 10, 10);
+        
+        
+        contexto.beginPath();
+        
+        contexto.moveTo(25+i,25);
+        contexto.lineTo(100+i,25);
+        
+        contexto.moveTo(25+i,25);
+        contexto.lineTo(20+i,45);
+        
+        contexto.moveTo(20+i,45);
+        contexto.lineTo(5+i,45);
+        
+        contexto.moveTo(5+i,45);
+        contexto.lineTo(5+i,70);
+        
+        contexto.moveTo(5+i,70);
+        contexto.lineTo(35+i,70);
+        
+        contexto.moveTo(60+i,70);
+        contexto.lineTo(100+i,70);
+        
+        contexto.moveTo(125+i,70);
+        contexto.lineTo(160+i,70);
+        
+        contexto.moveTo(160+i,70);
+        contexto.lineTo(165+i,60);
+        
+        contexto.moveTo(165+i,60);
+        contexto.lineTo(165+i,50);
+        
+        contexto.moveTo(165+i,50);
+        contexto.lineTo(115+i,40);
+        
+        contexto.moveTo(115+i,40);
+        contexto.lineTo(93+i,40);
+        
+        contexto.moveTo(93+i,40);
+        contexto.lineTo(100+i,25);
+        
+        contexto.fill();
+        
+        contexto.arc(48+i,70,12,0,Math.PI*2,true);
+        contexto.stroke();
+        contexto.beginPath();
+        contexto.arc(112+i,70,12,0,Math.PI*2,true);
+        contexto.stroke();
+
+        i = i +10;
+}
+
+
